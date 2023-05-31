cbda4e7a4eb8dbc0150453da7a5f687a21fcf9bd
Mekhi M <mmcfowler@brooklynsteamcenter.org>
2023-05-31 13:24:03 -0400

    12

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,77 @@
 <template>
-   <v-app id="inspire">
-     <v-app-bar flat>
-       <v-container class="fill-height d-flex align-center">
-         <v-avatar
-           class="me-10 ms-4"
-           color="grey-darken-1"
-           size="32"
-         ></v-avatar>
- 
-         <v-btn
-           v-for="link in links"
-           :key="link"
-           variant="text"
-         >
-           {{ link }}
-         </v-btn>
- 
-         <v-spacer></v-spacer>
- 
-         <v-responsive max-width="260">
-           <v-text-field
-             density="compact"
-             hide-details
-             variant="solo"
-           ></v-text-field>
-         </v-responsive>
-       </v-container>
-     </v-app-bar>
- 
-     <v-main class="bg-grey-lighten-3">
-       <v-container>
-         <v-row>
-           <v-col cols="2">
-             <v-sheet rounded="lg">
-               <v-list rounded="lg">
-                 <v-list-item
-                   v-for="n in 5"
-                   :key="n"
-                   link
-                 >
-                   <v-list-item-title>
-                     List Item {{ n }}
-                   </v-list-item-title>
-                 </v-list-item>
- 
-                 <v-divider class="my-2"></v-divider>
- 
-                 <v-list-item
-                   link
-                   color="grey-lighten-4"
-                 >
-                   <v-list-item-title>
-                     Refresh
-                   </v-list-item-title>
-                 </v-list-item>
-               </v-list>
-             </v-sheet>
-           </v-col>
- 
-           <v-col>
-             <v-sheet
-               min-height="70vh"
-               rounded="lg"
-             >
-               <!--  -->
-             </v-sheet>
-           </v-col>
-         </v-row>
-       </v-container>
-     </v-main>
-   </v-app>
- </template>
- 
- <script>
-   export default {
-     data: () => ({
-       links: [
-         'Dashboard',
-         'Messages',
-         'Profile',
-         'Updates',
-       ],
-     }),
-   }
- </script>
+  <v-app app >
+   <!--  <v-system-bar lights-out> </v-system-bar> --> 
+      <router-view />
+    <footer-comp />
+  </v-app>
+</template>
+
+<script>
+import { mapState, mapActions, mapGetters } from "vuex";
+import FooterComp from "./components/footer/FooterComp.vue";
+export default {
+  name: "App",
+  components: {
+    FooterComp,
+  },
+  data: () => ({
+    //
+  }),
+  computed: {
+    ...mapState(["playlist"]),
+    ...mapGetters(["listTracks"]),
+  },
+  methods: {
+    ...mapActions(["getPlaylist"]),
+  },
+  created() {
+    this.getPlaylist();
+    this.playlist;
+    this.listTracks;
+  },
+};
+</script>
+<style >
+a{
+  text-decoration: none;
+}
+.styleOverflow{
+  
+    /* border: 1px solid #aaa; */
+    height: 800px;
+    
+    /* max-width: 400px; */
+    /* background: #f1f2f3; */
+    overflow:auto;
+    box-sizing: border-box;
+    padding:0 1rem;
+}
+
+/* Estilos para motores Webkit y blink (Chrome, Safari, Opera... )*/
+
+.styleOverflow::-webkit-scrollbar {
+    -webkit-appearance: none;
+}
+
+.styleOverflow::-webkit-scrollbar:vertical {
+    width:10px;
+}
+
+.styleOverflow::-webkit-scrollbar-button:increment,.styleOverflow::-webkit-scrollbar-button {
+    display: none;
+} 
+
+.styleOverflow::-webkit-scrollbar:horizontal {
+    height: 10px;
+}
+
+.styleOverflow::-webkit-scrollbar-thumb {
+    background-color: #19516b;
+    border-radius: 20px;
+    border: 2px solid #d1dce1;
+}
+
+.styleOverflow::-webkit-scrollbar-track {
+    border-radius: 10px;  
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
+import Vue from 'vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
 
 
 // Vuetify
