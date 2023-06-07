03dbdd2d7a2553f7163885b4d779ba291afdda20
Mekhi M <mmcfowler@brooklynsteamcenter.org>
2023-06-07 14:53:08 -0400

    3

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,8 +2,33 @@
 </script>
 
 <template>
-    <h1>hello world</h1>
+    <v-card>
+        <v-card-title>
+            <h1 class="display-1">Welcome to Harmonize, the Future of Music</h1>
+        </v-card-title>
+        <v-card-text>
+            <p class="subtitle-2">
+                Harmonize is an innovative music website that leverages the vast resources and capabilities of the FreeSound
+                API to revolutionize the way musicians create and collaborate. By tapping into the extensive sound library
+                provided by FreeSound, Harmonize offers musicians a comprehensive platform to explore, discover, and
+                integrate high-quality sound samples into their musical creations.
+            </p>
+        </v-card-text>
+        <v-card-text>
+            <p class="subtitle-1">
+                At the core of Harmonize is its seamless integration with the FreeSound API, which allows musicians to
+                search, preview, and download sounds directly from within the platform. The intuitive search functionality
+                ensures that users can easily find the perfect sound to complement their musical ideas. Whether they're
+                looking for a particular instrument, a specific ambiance, or a unique percussive element, Harmonize empowers
+                musicians to explore and incorporate a diverse range of sounds into their compositions. </p>
+        </v-card-text>
+        <v-card-text>
+            <p class="subtitle-1">
+                We are excited to introduce a new era in music streaming.
+                Discover, explore, and enjoy an unrivaled collection of music at your fingertips.
+            </p>
+        </v-card-text>
+    </v-card>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -1,3 +1,14 @@
+<template>
+  <v-list-item>
+    <v-list-item-title>
+      <v-text-field v-model="query" label="Search" @keydown.enter="search" />
+    </v-list-item-title>
+  </v-list-item>
+  <v-list-item v-for="result in results" :key="result.id" link @click="downloadSound(result.id)">
+    <v-list-item-title>{{ result.name }}</v-list-item-title>
+  </v-list-item>
+</template>
+
 <script>
 export default {
   data() {
@@ -17,26 +28,17 @@ export default {
           console.error(error);
         });
     },
-    asyncFuntion() {
-      fetch("https://freesound.org/apiv2/search/text/?query=piano&token=mza7IVFXUJIu9hexCX0P0PgGiwrFZT6VO0oESxvM")
-        .then(response => {
-          console.log(response);
-        });
+    downloadSound(soundId) {
+      const downloadUrl = `https://freesound.org/apiv2/sounds/${soundId}/download/`;
+      const a = document.createElement('a');
+      a.href = downloadUrl;
+      a.download = `sound-${soundId}.mp3`;
+      a.target = '_blank';
+      a.click();
     }
   }
-}
+};
 </script>
-
-<template>
-<v-list-item>
-    <v-list-item-title>
-      <v-text-field v-model="searchText" label="Search" @keydown.enter="search" />
-    </v-list-item-title>
-</v-list-item>
-<v-list-item v-for="result in results" :key="result.id" link>
-                  <v-list-item-title>{{ result.name }}</v-list-item-title>
-                </v-list-item>
-</template>
 
 <style scoped>
 </style>
